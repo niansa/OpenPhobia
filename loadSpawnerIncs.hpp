50f3b41a6340e59c3f3f72ae8bcec3348eff8d08
niansa <anton-sa@web.de>
2021-12-30 22:52:04 +0100

    Added loadSpawnerIncs.hpp to gitignore

--- loadSpawnerIncs.hpp
+++
@@ -1,4 +0,0 @@
-#include "/mnt/hhdd/Programme/OSS/scatter/Scripts/Bullet.hpp"
-#include "/mnt/hhdd/Programme/OSS/scatter/Scripts/Jumper.hpp"
-#include "/mnt/hhdd/Programme/OSS/scatter/Scripts/Player.hpp"
-
